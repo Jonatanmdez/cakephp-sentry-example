0b545990d0489965e548a637bf105f0b3645cd46
Jonatan Menendez Canal <jmenendez@optimizaclick.com>
2018-03-29 12:30:43 +0200

    Instaled Connehito/cake-sentry plugin for sentry integration

--- cakephp/config/bootstrap.php
+++ cakephp/config/bootstrap.php
@@ -214,3 +214,9 @@ Type::build('timestamp')
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
+
+
+
+
+
+Plugin::load('Connehito/CakeSentry', ['bootstrap' => true]);
